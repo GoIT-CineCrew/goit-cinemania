64e5c8f46a7bb50a8f75519abbdcaf0f03a3e113
Dogukan-Ars <arss.dogukan@gmail.com>
2025-11-24 09:46:56 +0300

    try to fix issues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  base: "/goit-cinemania/",
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    base: '/goit-cinemania/', // 📌 Github pages base URL
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({ sort: 'mobile-first' }),
+    ],
+  };
 });
